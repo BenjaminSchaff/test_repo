5bca30f641c1dfbe722ae3f5634bd5316f96f20f
Benjamin Schaff <benjamin.schaff@maine.edu>
2018-06-06 11:06:36 -0400

    yay, branch done

--- hello.c
+++ hello.c
@@ -5,6 +5,6 @@ int main(int argc, char *argv[])
 	printf("Hello world\n");
 	printf("master");
 	
-	printf("testing branch.  Testing 2: Test harder\n");
+	printf("testing branch 2.2.  Testing 2: Test harder\n");
 	return 0;
 }
